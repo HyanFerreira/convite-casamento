c979547f3dd20e12c1c7991734cf7dd9e3f78da6
Hyan Ferreira <hcgamesmg2016@gmail.com>
2024-06-27 23:49:41 -0300

    update giftList

--- js/gift-list.js
+++ js/gift-list.js
@@ -2,129 +2,161 @@ const giftList = [
   {
     id: 1,
     name: 'Liquidificador',
+    selected: false,
   },
   {
     id: 2,
     name: 'Batedeira',
+    selected: true,
   },
   {
     id: 3,
     name: 'Garrafa de água e suco (vidro)',
+    selected: false,
   },
   {
     id: 4,
     name: 'Garrafa de café (azul bebê ou vinho)',
+    selected: false,
   },
   {
     id: 5,
     name: 'Potes de vidro',
+    selected: false,
   },
   {
     id: 6,
     name: 'Tábua de carne (vidro)',
+    selected: false,
   },
   {
     id: 7,
     name: 'Jogo de prato',
+    selected: false,
   },
   {
     id: 8,
     name: 'Ralador',
+    selected: false,
   },
   {
     id: 9,
     name: 'Escorredor de pratos',
+    selected: false,
   },
   {
     id: 10,
     name: 'Espátula e concha de silicone',
+    selected: false,
   },
   {
     id: 11,
     name: 'Forma de gelo de silicone',
+    selected: false,
   },
   {
     id: 12,
     name: 'Formas de bolo retangular e redonda',
+    selected: false,
   },
   {
     id: 13,
     name: 'Saladeira',
+    selected: false,
   },
   {
     id: 14,
     name: 'Jogo de sobremesa',
+    selected: false,
   },
   {
     id: 15,
     name: 'Canecas',
+    selected: false,
   },
   {
     id: 16,
     name: 'Marinex (sem tampa)',
+    selected: false,
   },
   {
     id: 17,
     name: 'Suporte de café (103)',
+    selected: false,
   },
   {
     id: 18,
     name: 'Potes de mantimentos (arroz, feijão)',
+    selected: false,
   },
   {
     id: 19,
     name: 'Pipoqueira',
+    selected: false,
   },
   {
     id: 20,
     name: 'Faqueiro',
+    selected: false,
   },
   {
     id: 21,
     name: 'Fruteira',
+    selected: false,
   },
   {
     id: 22,
     name: 'Descanso de panela',
+    selected: false,
   },
   {
     id: 23,
     name: 'Abridor de lata',
+    selected: false,
   },
   {
     id: 24,
     name: 'Espremedor de limão e laranja',
+    selected: false,
   },
   {
     id: 25,
     name: 'Ferro de passar roupa',
+    selected: false,
   },
   {
     id: 26,
     name: 'Mop spray',
+    selected: false,
   },
   {
     id: 27,
     name: 'Vassoura, rodo e pá',
+    selected: false,
   },
   {
     id: 28,
     name: 'Balde de limpeza (preto)',
+    selected: false,
   },
   {
     id: 29,
     name: 'Tábua de passar roupa',
+    selected: false,
   },
   {
     id: 30,
     name: 'Pano de chão',
+    selected: false,
   },
   {
     id: 31,
     name: 'Tapete de cozinha',
+    selected: false,
   },
   {
     id: 32,
     name: 'Panos de prato',
+    selected: false,
   },
 ];
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Seleção de elementos iniciais
   const clickConvite = document.querySelector('.click-convite');
   const envelope = document.querySelector('.envelope-wrapper');
   const envelopeSup = document.querySelector('.envelope-wrapper-sup');
@@ -10,18 +11,19 @@ document.addEventListener('DOMContentLoaded', function() {
   const overflowIndex = document.querySelector('.overflow-index');
   const animateElements = document.querySelectorAll('.animate');
   const heartIndex = document.querySelector('.heart-index');
-  let btnAudio = document.getElementById('btnAudio');
-  let audio = document.getElementById('audio');
-
+  const btnAudio = document.getElementById('btnAudio');
+  const audio = document.getElementById('audio');
   const click = document.querySelector('.click');
   const pulse = document.querySelector('.pulse');
   const pulseSpans = document.querySelectorAll('.pulse span');
-
   const openGiftList = document.querySelector('.openGiftList');
   const modal = document.querySelector('.modal');
   const contentGift = document.querySelector('.content-gift');
   const btnBack = document.querySelector('.btnBack');
+  const btnConfirmGift = document.querySelector('.btnConfirmGift');
+  const alert = document.querySelector('.alert');
 
+  // Eventos de abertura e fechamento da lista de presentes
   openGiftList.addEventListener('click', () => {
     modal.classList.add('active');
     setTimeout(() => {
@@ -38,15 +40,18 @@ document.addEventListener('DOMContentLoaded', function() {
     }, 600);
   });
 
+  // Animações iniciais ao carregar a página
   setTimeout(() => {
     click.classList.add('active');
   }, 1200);
+
   setTimeout(() => {
     pulse.classList.add('active');
     pulseSpans.forEach(span => {
       span.classList.add('active');
     });
   }, 1800);
+
   setTimeout(() => {
     pulse.classList.remove('active');
     click.classList.remove('active');
@@ -55,6 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }, 4000);
 
+  // Evento ao clicar no convite
   clickConvite.addEventListener('click', () => {
     envelope.classList.add('flap');
     envelopeSup.classList.add('flap');
@@ -63,31 +69,103 @@ document.addEventListener('DOMContentLoaded', function() {
     envelopeBg.classList.add('active');
     selo.classList.add('active');
     btnAudio.style.right = '0';
+
     setTimeout(() => {
       audio.play();
     }, 500);
+
     setTimeout(() => {
       envelopeSup.classList.add('z-index-zero');
     }, 1000);
+
     setTimeout(() => {
       container.classList.add('overflow');
     }, 1600);
+
     setTimeout(() => {
       heartIndex.classList.add('overflow');
     }, 1800);
+
     setTimeout(() => {
       heartIndex.style.display = 'none';
       main.classList.add('active');
       main.style.padding = '0';
     }, 2200);
+
     setTimeout(() => {
       containerConvite.style.opacity = '1';
       overflowIndex.classList.add('active');
     }, 3000);
+
     animateElements.forEach((element, index) => {
       setTimeout(() => {
         element.classList.add('active');
       }, 3600 + index * 300);
     });
   });
+
+  // Funções relacionadas à lista de presentes
+  function cloneGift() {
+    const c = el => document.querySelector(el);
+
+    giftList.forEach((item, index) => {
+      let giftItem = c('.model-gift').cloneNode(true);
+      let checkbox = giftItem.querySelector('.gift-checkbox');
+      let span = giftItem.querySelector('.gift-span');
+      let gift = giftItem.querySelector('.gift');
+
+      span.innerHTML = `${index + 1}. ${item.name}`;
+
+      if (item.selected) {
+        giftItem.classList.add('selected'); // Adiciona a classe 'selected' se o item estiver selecionado
+      }
+
+      checkbox.addEventListener('click', function(event) {
+        event.stopPropagation();
+      });
+
+      gift.addEventListener('click', function() {
+        checkbox.checked = !checkbox.checked;
+        giftList[index].selected = checkbox.checked; // Atualiza o estado de seleção do item
+      });
+
+      c('.content-gift .content-gift-list .content-cozinha').append(giftItem);
+    });
+  }
+
+  function getSelectedItems() {
+    const selectedItems = [];
+    const gifts = document.querySelectorAll('.content-cozinha .model-gift');
+
+    gifts.forEach((gift, index) => {
+      const checkbox = gift.querySelector('.gift-checkbox');
+      if (checkbox.checked) {
+        selectedItems.push(giftList[index]);
+      }
+    });
+
+    return selectedItems;
+  }
+
+  function showAlert() {
+    alert.style.top = '0';
+
+    setTimeout(() => {
+      alert.style.top = '-50px';
+    }, 3000);
+  }
+
+  // Evento ao clicar no botão para confirmar presentes
+  btnConfirmGift.addEventListener('click', function() {
+    const selectedItems = getSelectedItems();
+    if (selectedItems.length === 0) {
+      showAlert();
+    } else {
+      console.log(selectedItems);
+      // Aqui você pode fazer o que quiser com a variável selectedItems
+    }
+  });
+
+  // Inicialização da lista de presentes
+  cloneGift();
 });
